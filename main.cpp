d21ec2db9d7d6ec41329d712885bfbecf7feee7b
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2022-12-07 23:25:03 -0400

    [Batalha Naval]

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "portuguese");
+	setlocale(LC_ALL, "en_US");
+
+	std::cout << " ----- [Batalha Naval] -----";
+
+	return 0;
+}
